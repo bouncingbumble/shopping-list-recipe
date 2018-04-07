8722c32f575b11717d9f6299ce18b7207fdbfc22
elongatedmuskrat <jordboudreau@gmail.com>
2018-04-07 10:47:04 -0700

    add recipe object and loop trhough in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'a test desc', 'https://www.iheartnaptime.net/wp-content/uploads/2016/08/pizza-740x740.jpg'),
+    new Recipe('A Test recipe', 'a test desc', 'https://www.iheartnaptime.net/wp-content/uploads/2016/08/pizza-740x740.jpg')
+  ];
 
   constructor() { }
 
