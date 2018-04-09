b6da7ac8f7a041bc9178c369365e27bfa9129016
elongatedmuskrat <jordboudreau@gmail.com>
2018-04-09 09:22:32 -0700

    add navigation tab logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  recipesNav: boolean;
+  shoppingNav: boolean;
+
+  detirmineNav(tab: string){
+    if(tab == "recipes"){
+      this.recipesNav = true;
+      this.shoppingNav = false;
+    }else if(tab == "shopping"){
+      this.recipesNav = false;
+      this.shoppingNav = true;
+    }else{
+      console.log(tab + " Is not a valid nav");
+    }
+
+  }
+
 }
